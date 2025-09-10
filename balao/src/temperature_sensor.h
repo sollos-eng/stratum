4df77a89f6367c94c14391aa38878c10a12810b0
Esther <76445804+sollos-eng@users.noreply.github.com>
2025-09-10 18:25:29 -0300

    Update temperature_sensor.h
    Atualizando o .h para a leitura de 3 sensores de temperatura.

--- balao/src/temperature_sensor.h
+++ balao/src/temperature_sensor.h
@@ -1,6 +1,24 @@
 #pragma once
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-void setupTemperatureSensors();
-bool readTemperatures(float &t1, float &t2, bool &heating);
+// Classe para gerenciar múltiplos sensores DS18B20
+class DS18B20Multi {
+  public:
+    // Construtor: recebe o pino onde os sensores estão conectados
+    DS18B20Multi(uint8_t pino);
 
+    // Inicializa os sensores
+    void iniciar();
+
+    // Lê a temperatura de um sensor pelo endereço
+    float lerTemperatura(const DeviceAddress device);
+
+    // Faz leitura de todos os sensores (exemplo: 3 sensores fixos)
+    void lerTodos(float &t1, float &t2, float &t3);
+
+  private:
+    OneWire oneWire;
+    DallasTemperature sensors;
+};
